0f60957f187c73abe1c3afac92865c0f078ac6e6
Hugo Iuri <hugo@minutrade.com>
2015-07-28 17:34:34 -0300

    criando directivas

--- app.js
+++ app.js
@@ -2,27 +2,20 @@
     var app = angular.module('gemStore', []);
 
     app.controller('StoreController', function() {
-        this.teste = "teste";
         this.products = gems;
     });
 
-    app.controller('TabController', function() {
-        this.tab = 1;
-
-        this.setTab = function(newValue) {
-            this.tab = newValue;
-        };
-
-        this.isSet = function(tabName) {
-            return this.tab === tabName;
-        };
-    });
-
-    app.controller('GalleryController', function() {
-        this.current = 0;
-
-        this.setCurrent = function(newGallery) {
-            this.current = newGallery || 0;
+    app.directive('productGallery', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-gallery.html',
+            controller: function() {
+                this.current = 0;
+                this.setCurrent = function(imageNumber) {
+                    this.current = imageNumber || 0;
+                };
+            },
+            controllerAs: 'gallery'
         };
     });
 
@@ -31,19 +24,55 @@
         this.review = {};
 
         this.addReview = function(product) {
-            this.review.createdOn = Date.now();
-
             product.reviews.push(this.review);
             this.review = {};
         };
 
     });
 
+    app.directive("productDescription", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-description.html"
+        };
+    });
+
+    app.directive("productReviews", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-reviews.html"
+        };
+    });
+
+    app.directive("productSpecs", function() {
+        return {
+            restrict: "A",
+            templateUrl: "product-specs.html"
+        };
+    });
+
+    app.directive("productTabs", function() {
+        return {
+            restrict: "E",
+            templateUrl: "product-tabs.html",
+            controller: function() {
+                this.tab = 1;
+
+                this.isSet = function(checkTab) {
+                    return this.tab === checkTab;
+                };
+
+                this.setTab = function(activeTab) {
+                    this.tab = activeTab;
+                };
+            },
+            controllerAs: "tab"
+        };
+    });
+
     var gems = [{
         name: 'Azurite',
         description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-        canPurchase: true,
-        soldOut: false,
         shine: 8,
         price: 110.50,
         rarity: 7,
@@ -57,19 +86,15 @@
         reviews: [{
             stars: 5,
             body: "I love this gem!",
-            author: "joe@example.org",
-            createdOn: 1397490980837
+            author: "joe@example.org"
         }, {
             stars: 1,
             body: "This gem sucks.",
-            author: "tim@example.org",
-            createdOn: 1397490980837
+            author: "tim@example.org"
         }]
     }, {
         name: 'Bloodstone',
         description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-        canPurchase: true,
-        soldOut: false,
         shine: 9,
         price: 22.90,
         rarity: 6,
@@ -83,19 +108,15 @@
         reviews: [{
             stars: 3,
             body: "I think this gem was just OK, could honestly use more shine, IMO.",
-            author: "JimmyDean@example.org",
-            createdOn: 1397490980837
+            author: "JimmyDean@example.org"
         }, {
             stars: 4,
             body: "Any gem with 12 faces is for me!",
-            author: "gemsRock@example.org",
-            createdOn: 1397490980837
+            author: "gemsRock@example.org"
         }]
     }, {
         name: 'Zircon',
         description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-        canPurchase: true,
-        soldOut: false,
         shine: 70,
         price: 1100,
         rarity: 2,
@@ -104,23 +125,20 @@
         images: [
             "images/gem-06.gif",
             "images/gem-07.gif",
-            "images/gem-10.gif"
+            "images/gem-08.gif"
         ],
         reviews: [{
             stars: 1,
             body: "This gem is WAY too expensive for its rarity value.",
-            author: "turtleguyy@example.org",
-            createdOn: 1397490980837
+            author: "turtleguyy@example.org"
         }, {
             stars: 1,
             body: "BBW: High Shine != High Quality.",
-            author: "LouisW407@example.org",
-            createdOn: 1397490980837
+            author: "LouisW407@example.org"
         }, {
             stars: 1,
             body: "Don't waste your rubles!",
-            author: "nat@example.org",
-            createdOn: 1397490980837
+            author: "nat@example.org"
         }]
     }];
-}())
+})();
